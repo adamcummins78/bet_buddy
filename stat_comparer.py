ce677dd6dc3b9746dc2b9a80919947ab8395d17e
Adam Cummins <adamrobertcummins@gmail.com>
2021-03-03 21:24:52 -0500

    added new lines commands and fixed some typos

--- stat_comparer.py
+++ stat_comparer.py
@@ -5,7 +5,7 @@ print("\n")
 print("======================================================")
 print("===================== BET BUDDY ======================")
 print("======================================================")
-print(f"Welcome to Bet Buddy, {user}! (College Footbal Edition)")
+print(f"Welcome to Bet Buddy, {user}! (College Football Edition)")
 print()
 print("It is WEEK ONE of the college football season.") #I want this section to print out what week of the college football season it is based of datetime entry
 print("======================================================")
@@ -16,12 +16,14 @@ team = input("Please enter the first team you would like to compare, or q to qui
 while(team == "q"):
     exit()
 team_two = input("Please enter the second team you would like to compare:  ")
+print("\n")
 
 
 while(team != "q" or team_two != "q"):
     print("Happy betting! Are there more teams you want to compare?""\n")
     team = input("Please enter the first team you would like to compare:  ")
     team_two = input("Please enter the second team you would like to compare, or q to quit:  ")
+    print("\n")
         # if(team == "UK"):
     
     

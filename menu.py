591e82240b1756ace33d81f073d35e66f34166ea
Adam Cummins <adamrobertcummins@gmail.com>
2021-03-25 20:16:17 -0400

    added team selection to menu screen

--- menu.py
+++ menu.py
@@ -15,12 +15,37 @@ def buildMenu():
         print("=" * 54)
         print("=" * 54)
         print("=" * 54)
-        print("Selection:\n1. Kentucky\n2. Gerogia\n3. Alabama\n4. Tennesse")
+        print("Selection:\n Kentucky (Enter: UK)\n Georgia (Enter: UGA)\n Alabama (Enter: AL)\n Tennesse (Enter: TN)")
 
-        team = input("Please enter the number for first team you would like to compare, or q to quit:  ")
-        if(team == "q"):
+        team = input("Please enter the number for first team you would like to compare, or q to quit:  ").upper()
+        if(team == "Q"):
             exit()
-        team_two = input("Please enter the second team you would like to compare:  ")
+        team_two = input("Please enter the second team you would like to compare:  ").upper()
+
+        
+        if (team == "UK"):
+            print(f"UK is averaging {ky_off} yards on offense so far this year and on average allowing {ky_def} yards on defense.")
+        elif (team == "UGA"):
+            print(f"Georgia is averaging {ga_off} yards on offense so far this year and on average allowing {ga_def} yards on defense.")
+        elif (team == "AL"):
+            print(f"Alabama is averaging {al_off} yards on offense so far this year and on average allowing {al_def} yards on defense.")
+        elif (team == "TN"):
+            print(f"Tennesse is averaging {ten_off} yards on offense so far this year and on average allowing {ten_def} yards on defense.")
+        else:
+            print("Invalid option")
+        
+
+        if (team_two == "UK"):
+            print(f"UK is averaging {ky_off} yards on offense so far this year and on average allowing {ky_def} yards on defense.")
+        elif (team_two == "UGA"):
+            print(f"Georgia is averaging {ga_off} yards on offense so far this year and on average allowing {ga_def} yards on defense.")
+        elif (team_two == "AL"):
+            print(f"Alabama is averaging {al_off} yards on offense so far this year and on average allowing {al_def} yards on defense.")
+        elif (team_two == "TN"):
+            print(f"Tennesse is averaging {ten_off} yards on offense so far this year and on average allowing {ten_def} yards on defense.")
+        else:
+            print("Invalid option")
+
         print("\n")
         print("Happy Betting!")
         print("\n")
